c1894b1077431b92ff17d630ceedb3b6b262ff15
Greg Price <greg@madebyhive.com>
2021-02-25 17:02:13 +0000

    Added dev notes

--- RxShop/Common/Logging/Resources.swift
+++ RxShop/Common/Logging/Resources.swift
@@ -7,6 +7,21 @@
 
 import RxSwift
 
+/*
+ https://github.com/ReactiveX/RxSwift/blob/main/Documentation/GettingStarted.md#debugging-memory-leaks
+ Most efficient way to test for memory leaks is:
+ 
+ - navigate to your screen and use it
+ - navigate back
+ - observe initial resource count
+ - navigate second time to your screen and use it
+ - navigate back
+  -observe final resource count
+ 
+ In case there is a difference in resource count between initial and final resource counts, there might be a memory leak somewhere.
+ The reason why 2 navigations are suggested is because first navigation forces loading of lazy resources.
+ */
+
 func logRxResources() {
     #if DEBUG
     _ = Observable<Int>.interval(.seconds(1), scheduler: MainScheduler.instance)
